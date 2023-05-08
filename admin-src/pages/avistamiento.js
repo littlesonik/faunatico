73cb219ad7f67ef3608bf38c4a7959aad15ec4d5
littlesonik <sonik.zapata@gmail.com>
2023-05-07 21:41:00 -0400

    correccion rutas

--- admin-src/pages/avistamiento.js
+++ admin-src/pages/avistamiento.js
@@ -1,11 +1,11 @@
 import AvistamientoManager from "@/components/avistamiento/manager";
 import Layout from "@/components/layout";
-import {verificacion} from "@/verificacion";
+import { verificacionUsuario } from "@/verificacion";
 import { useEffect } from "react";
 
 export default function PaginaAvistamiento(){
   useEffect(() => {
-    verificacion();
+    verificacionUsuario();
   });
 
   return(
--- admin-src/verificacion.js
+++ admin-src/verificacion.js
@@ -1,28 +1,32 @@
-//recuperar el token
-export const storageUsuario = localStorage.getItem("usuario"); //viene como string
-if( storageUsuario == null){
-  //si no esta autenticado, lo envio a la pagina de login
-  window.location = "login.html"
-}
+export const verificarUsuario = async () => {
 
-const objetoUsuario = JSON.parse(storageUsuario); //lo convierto a formato json
-const token = objetoUsuario.user.stsTokenManager.accessToken; //esta ruta es para obtener el token, se recomienda revisar la ruta para no equivocarse
-
-console.log(token);
-
-const baseUrl = getBaseUrl();
-const url = baseUrl + '/usuario/checktoken';
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + token
-  },
-}).then( respuesta => {
-  if(!respuesta.ok){
-    throw new Error("Token no valido")
+  //recuperar el token
+  const storageUsuario = localStorage.getItem("usuario"); //viene como string
+  if( storageUsuario == null){
+    //si no esta autenticado, lo envio a la pagina de login
+    window.location = "login.html"
   }
-  respuesta.json();
-}).catch (error =>{
-  window.location = 'login.html';
-});
+
+  const objetoUsuario = JSON.parse(storageUsuario); //lo convierto a formato json
+  const token = objetoUsuario.user.stsTokenManager.accessToken; //esta ruta es para obtener el token, se recomienda revisar la ruta para no equivocarse
+
+  console.log(token);
+
+  const baseUrl = getBaseUrl();
+  const url = baseUrl + '/usuario/checktoken';
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  }).then( respuesta => {
+    if(!respuesta.ok){
+      throw new Error("Token no valido")
+    }
+    respuesta.json();
+  }).catch (error =>{
+    window.location = 'login.html';
+  });
+}
