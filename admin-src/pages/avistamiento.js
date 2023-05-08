ecf8951feeef3bc586a23195ba1c78d2263cda0b
littlesonik <sonik.zapata@gmail.com>
2023-05-07 20:57:32 -0400

    correccion rutas

--- admin-src/pages/avistamiento.js
+++ admin-src/pages/avistamiento.js
@@ -1,6 +1,7 @@
 import AvistamientoManager from "@/components/avistamiento/manager";
 import Layout from "@/components/layout";
 import {verificacion} from "@/verificacion";
+import { useEffect } from "react";
 
 export default function PaginaAvistamiento(){
   useEffect(() => {
